29daa22868a966ee0e07a8e1c64bb224118e829c
Carl Olson <carl.anders.olson@gmail.com>
2017-04-22 16:24:26 -0500

    start implementing uglysvg

--- ugly-svg/src/UglySVG.hx
+++ ugly-svg/src/UglySVG.hx
@@ -1,6 +1,31 @@
+import sys.io.File;
 
 class UglySVG {
 
+	private var xml : Xml;
 
+	private function new(xml : Xml) {
+		this.xml = xml;
+	}
+
+	public static function create(path : String) : UglySVG {
+		try {
+			var content = File.getContent(path);
+			var xml = Xml.parse(content);
+			return new UglySVG(xml);
+		} catch(error : Dynamic) {
+			return null;
+		}
+	}
+
+	public function uglify() {
+		for(path in xml.firstElement().elementsNamed('path')) {
+			path.set('d', '!');
+		}
+	}
+
+	public function toString() : String {
+		return xml.toString();
+	}
 
 }
--- ugly-svg/test/TestUglySVG.hx
+++ ugly-svg/test/TestUglySVG.hx
@@ -3,7 +3,31 @@ import utest.Assert;
 class TestUglySVG {
   public function new() {};
 
+  var svg : UglySVG;
+
   public function setup() {
+	  svg = UglySVG.create('heart.svg');
+  }
+
+  public function test_accepts_valid_svg_files() {
+	  Assert.notNull(svg);
+  }
+
+  public function skip_rejects_invalid_svg_files() {
+	  svg = UglySVG.create('bogus.svg');
+	  Assert.isTrue(svg == null);
+  }
+
+  public function test_can_output_svgs_string() {
+	  Assert.match(~/^<svg/i, svg.toString());
+  }
+
+  public function test_removes_cubic_bezier_curves() {
+	  svg.uglify();
+	  var xml = Xml.parse(svg.toString());
+	  for(path in xml.firstElement().elementsNamed('path')) {
+	  	  Assert.match(~/^[^Cc]+$/, path.get('d'));
+	  }
   }
 
   public function teardown() {
