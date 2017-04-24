0b9b181aaf3bc336072f7265a6ef4a9dba565517
Carl Olson <carlolson@users.noreply.github.com>
2017-04-24 12:17:13 -0500

    fix code style

--- ugly-svg/src/UglySVG.hx
+++ ugly-svg/src/UglySVG.hx
@@ -1,31 +1,30 @@
 import sys.io.File;
 
 class UglySVG {
+    private var xml:Xml;
 
-	private var xml : Xml;
+    private function new(xml:Xml) {
+	this.xml = xml;
+    }
 
-	private function new(xml : Xml) {
-		this.xml = xml;
+    public static function create(path:String):UglySVG {
+	try {
+	    var content = File.getContent(path);
+	    var xml = Xml.parse(content);
+	    return new UglySVG(xml);
+	} catch(error : Dynamic) {
+	    return null;
 	}
+    }
 
-	public static function create(path : String) : UglySVG {
-		try {
-			var content = File.getContent(path);
-			var xml = Xml.parse(content);
-			return new UglySVG(xml);
-		} catch(error : Dynamic) {
-			return null;
-		}
+    public function uglify() {
+	for(path in xml.firstElement().elementsNamed('path')) {
+	    trace(path.get('d'));
+	    path.set('d', '!');
 	}
+    }
 
-	public function uglify() {
-		for(path in xml.firstElement().elementsNamed('path')) {
-			path.set('d', '!');
-		}
-	}
-
-	public function toString() : String {
-		return xml.toString();
-	}
-
+    public function toString():String {
+	return xml.toString();
+    }
 }
