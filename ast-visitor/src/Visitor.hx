e87764ccedc25c10241eb20f5cdee548fc4ebeab
Carl Olson <carlolson@users.noreply.github.com>
2017-04-24 11:21:52 -0500

    add visitConst, visitArray, and visitBinop

--- ast-visitor/src/Visitor.hx
+++ ast-visitor/src/Visitor.hx
@@ -1,11 +1,77 @@
-import haxe.macro.*;
+using haxe.macro.Expr;
 
 class Visitor<T> {
 
     public function new() {
     }
 
-    public function visit(expr : Expr) : Maybe<T> {
+    public function visit(expr:Expr):Maybe<T> {
+	switch(expr.expr) {
+	case EConst( c ):
+	    return visitConst(c);
+	case EArray( e1, e2 ):
+	    return visitArray(e1, e2);
+	case EBinop( op, e1, e2 ):
+	    return visitBinop(op, e1, e2);
+	case EField( e, field ):
+
+	case EParenthesis( e ):
+
+	case EObjectDecl( fields ):
+
+	case EArrayDecl( values ):
+
+	case ECall( e, params ):
+
+	case ENew( t, params ):
+
+	case EUnop( op, postFix, e ):
+
+	case EVars( vars ):
+
+	case EFunction( name, f ):
+
+	case EBlock( exprs ):
+
+	case EFor( it, expr ):
+
+	case EIn( e1, e2 ):
+
+	case EIf( econd, eif, eelse ):
+
+	case EWhile( econd, e, normalWhile ):
+
+	case ESwitch( e, cases, edef ):
+
+	case ETry( e, catches ):
+
+	case EReturn( e ):
+
+	case EBreak:
+
+	case EContinue:
+
+	case EUntyped( e ):
+
+	case EThrow( e ):
+
+	case ECast( e, t ):
+
+	case EDisplay( e, isCall ):
+
+	case EDisplayNew( t ):
+
+	case ETernary( econd, eif, eelse ):
+
+	case ECheckType( e, t ):
+
+	case EMeta( s, e ):
+
+	}
+	return None;
+    }
+
+    public function visitChildren(expr : Expr) : Maybe<T> {
 	switch(expr.expr) {
 	case EConst( c ):
 
@@ -68,6 +134,18 @@ class Visitor<T> {
 	case EMeta( s, e ):
 
 	}
+	return None;
+    }
+
+    public function visitConst(const:Constant):Maybe<T> {
+	return None;
+    }
+
+    public function visitArray(e1:Expr, e2:Expr):Maybe<T> {
+	return None;
+    }
+
+    public function visitBinop(op:Binop, e1:Expr, e2:Expr):Maybe<T> {
 	return None;
     }
 }
--- ast-visitor/test/TestVisitor.hx
+++ ast-visitor/test/TestVisitor.hx
@@ -1,4 +1,30 @@
 import utest.Assert;
+import Maybe;
+using haxe.macro.Expr;
+
+class MyTestVisitor extends Visitor<Int> {
+    override public function visitConst(const : Constant) : Maybe<Int> {
+	switch(const) {
+	case CInt(v):
+	    return Just(Std.parseInt(v));
+	default:
+	    return None;
+	}
+    }
+
+    override public function visitArray(e1:Expr, e2:Expr) : Maybe<Int> {
+	switch(e2.expr) {
+	case EConst(CInt(v)):
+	    return Just(Std.parseInt(v));
+	default:
+	    return None;
+	}
+    }
+
+    override public function visitBinop(op: Binop, e1: Expr, e2: Expr) : Maybe<Int> {
+	return MaybeUtil.map(visit(e1), visit(e2), function(a, b) return Just(a + b));
+    }
+}
 
 class TestVisitor {
     var visitor : Visitor<Int>;
@@ -9,16 +35,41 @@ class TestVisitor {
 	visitor = new Visitor<Int>();
     }
 
+    public function teardown() {
+    }
+
     public function test_visit_returns_none_by_default() {
 	var input = macro 0;
 	switch visitor.visit(input) {
-            case Just(_):
-		Assert.fail();
-            case None:
-		Assert.pass();
-	}
+		case Just(_):
+		    Assert.fail();
+		    case None:
+			Assert.pass();
+	    }
     }
 
-    public function teardown() {
+    public function test_visitChildren_returns_none_by_default() {
+	var input = macro 0 + 0;
+	switch visitor.visitChildren(input) {
+		case Just(_):
+		    Assert.fail();
+		    case None:
+			Assert.pass();
+	    }
+    }
+
+    public function test_visit_calls_visitConst() {
+	visitor = new MyTestVisitor();
+	Assert.same(Just(0), visitor.visit(macro 0));
+    }
+
+    public function test_visit_calls_visitArray() {
+	visitor = new MyTestVisitor();
+	Assert.same(Just(5), visitor.visit(macro array[5]));
+    }
+
+    public function test_visit_calls_visitBinop() {
+	visitor = new MyTestVisitor();
+	Assert.same(Just(2), visitor.visit(macro 1 + 1));
     }
 }
