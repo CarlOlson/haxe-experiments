eddbfa55a479606b0683cb46f734f8da850f4c90
Carl Olson <carlolson@users.noreply.github.com>
2017-04-26 14:18:37 -0500

    implement start of linearizeCubic

--- ugly-svg/src/UglySVG.hx
+++ ugly-svg/src/UglySVG.hx
@@ -1,4 +1,5 @@
 import sys.io.File;
+import SvgPath;
 
 class UglySVG {
     private var xml:Xml;
@@ -25,5 +26,56 @@ class UglySVG {
 
     public function toString():String {
 	return xml.toString();
+    }
+
+    private static function linearizeCubic(kind:Kind, startPoint:Point, startCtrl:Point,
+					   endCtrl:Point, endPoint:Point, minLength:Float):Array<Path> {
+	var pathLength:Float;
+
+	if (kind == Relative) {
+	    startPoint = {x: 0.0, y: 0.0};
+	    pathLength = cubicLength({x: 0.0, y: 0.0}, startCtrl, endCtrl, endPoint);
+	} else {
+	    pathLength = cubicLength(startPoint, startCtrl, endCtrl, endPoint);
+	}
+
+	if (pathLength < minLength) {
+	    return [Line(kind, endPoint)];
+	}
+
+	return null;
+    }
+
+    private static function cubicLength(startPoint:Point, startCtrl:Point, endCtrl:Point, endPoint:Point):Float {
+	var lastPoint = startPoint,
+	    length = 0.0,
+	    n = 16;
+
+	for (i in 1...(n+1)) {
+	    var currPoint = cubicPoint(i / n, startPoint, startCtrl, endCtrl, endPoint);
+	    length += distance(lastPoint, currPoint);
+	    lastPoint = currPoint;
+	}
+
+	return length;
+    }
+
+    private static function cubicPoint(t:Float, startPoint:Point, startCtrl:Point, endCtrl:Point, endPoint:Point):Point {
+	var dt = 1 - t,
+	    dt2 = dt * dt,
+	    dt3 = dt2 * dt,
+	    t2 = t * t,
+	    t3 = t2 * t,
+	    x = startPoint.x * dt3 + startCtrl.x * 3 * dt2 * t + endCtrl.x * 3 * dt * t2 + endPoint.x * t3,
+	    y = startPoint.y * dt3 + startCtrl.y * 3 * dt2 * t + endCtrl.y * 3 * dt * t2 + endPoint.y * t3;
+
+	return {x: x, y: y};
+    }
+
+    private static function distance(a:Point, b:Point):Float {
+	var dx = (a.x - b.x),
+	    dy = (a.y - b.y);
+
+	return Math.sqrt(dx * dx + dy * dy);
     }
 }
--- ugly-svg/test/TestUglySVG.hx
+++ ugly-svg/test/TestUglySVG.hx
@@ -1,7 +1,14 @@
 import utest.Assert;
+import SvgPath;
 
+@:access(UglySVG)
 class TestUglySVG {
     var svg:UglySVG;
+
+    var startPoint = {x:120.0, y:160.0};
+    var startCtrl = {x:35.0, y:200.0};
+    var endCtrl = {x:220.0, y:260.0};
+    var endPoint = {x:220.0, y:40.0};
 
     public function new() {};
 
@@ -30,5 +37,41 @@ class TestUglySVG {
 	for(path in xml.firstElement().elementsNamed('path')) {
 	    Assert.match(~/^[^Cc]+$/, path.get('d'));
 	}
+    }
+
+    public function test_linearize_minimum_length() {
+	var p1 = {x:1.0, y:1.0},
+	    p2 = {x:0.0, y:0.0},
+	    p3 = {x:1.0, y:0.0};
+
+	var minLength = 1.5;
+
+        Assert.same([Line(Absolute, {x:1, y:0})],
+		    UglySVG.linearizeCubic(Absolute, {x:0.0, y:0.0}, p1, p2, p3, minLength));
+
+	Assert.same([Line(Relative, {x:1, y:0})],
+		    UglySVG.linearizeCubic(Relative, {x:0.0, y:0.0}, p1, p2, p3, minLength));
+    }
+
+    public function test_distance() {
+	Assert.equals(UglySVG.distance({x:0.0, y:0.0}, {x:1.0, y:0.0}),
+		      1.0);
+	Assert.floatEquals(UglySVG.distance({x:0.0, y:0.0}, {x:2.0, y:3.0}),
+			   Math.sqrt(13));
+    }
+
+    public function test_cubicLength() {
+	var length = UglySVG.cubicLength(startPoint, startCtrl, endCtrl, endPoint);
+
+	Assert.floatEquals(272.87, length, 0.65);
+    }
+
+    public function test_cubicPoint() {
+	function pointAtTime(t) {
+	    return UglySVG.cubicPoint(t, startPoint, startCtrl, endCtrl, endPoint);
+	}
+
+	Assert.same(startPoint, pointAtTime(0), 'equals start point at t = 0');
+	Assert.same(endPoint, pointAtTime(1.0), 'equals end point at t = 1');
     }
 }
