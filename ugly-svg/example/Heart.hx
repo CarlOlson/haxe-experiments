dc5ee28d162a99b671c578bc519786859b3a0321
Carl Olson <carlolson@users.noreply.github.com>
2017-04-26 22:28:53 -0500

    add example

---
+++ ugly-svg/example/Heart.hx
@@ -0,0 +1,22 @@
+package;
+
+class Heart {
+    public static function main() {
+	var args = Sys.args();
+
+	if (args == null || args.length == 0) {
+	    Sys.print('File not given');
+	    return;
+	}
+
+	var filename = args[args.length - 1];
+
+	if (sys.FileSystem.exists(filename)) {
+	    var svg = UglySVG.create(filename);
+	    svg.uglify(25);
+	    Sys.print(svg.toString());
+	} else {
+	    Sys.print('File $filename does not exist!');
+	}
+    }
+}
