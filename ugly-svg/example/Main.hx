8462125849e3f8a3fdd4dc64d7e7d68477ffb04a
Carl Olson <carlolson@users.noreply.github.com>
2017-04-28 14:06:15 -0500

    update example

--- ugly-svg/example/Main.hx
+++ ugly-svg/example/Main.hx
@@ -1,5 +1,7 @@
 package;
 
+import SvgPath;
+
 class Main {
     public static function main() {
 	var args = Sys.args();
@@ -13,10 +15,32 @@ class Main {
 
 	if (sys.FileSystem.exists(filename)) {
 	    var svg = UglySVG.create(filename);
-	    svg.uglify(50);
+	    svg.uglify(10 - Std.random(4));
+	    svg.transformPaths(jitter);
 	    Sys.print(svg.toString());
 	} else {
 	    Sys.print('File $filename does not exist!');
 	}
+    }
+
+    public static function jitterPoint(point:Point):Point {
+	var dx = (Std.random(1000) / 250) - 2,
+	    dy = (Std.random(1000) / 250) - 2;
+	return {x: point.x + dx, y: point.y + dy};
+    }
+
+    public static function jitter(path:Array<Path>):Array<Path> {
+	var jitterPath = [];
+
+	for (cmd in path) {
+	    switch(cmd) {
+	    case Line(kind, point):
+		jitterPath.push(Line(kind, jitterPoint(point)));
+	    default:
+		jitterPath.push(cmd);
+	    }
+	}
+
+	return jitterPath;
     }
 }
